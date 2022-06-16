3539d47b5cbe6e33c5665618b6935aec5e63715d
Askary Abbas <dev.askary@gmail.com>
2022-06-16 23:50:21 +0530

    Added Composer support.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,10 @@
  * @package Simple_Alert_Plugin
  */
 
+if ( class_exists( '\Yoast\PHPUnitPolyfills\Autoload' ) === false ) {
+	require_once 'vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+}
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
