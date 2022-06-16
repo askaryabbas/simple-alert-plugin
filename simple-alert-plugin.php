9bbfa03fc4bca0bac7d33fa72a616f31a72af2a5
Askary Abbas <dev.askary@gmail.com>
2022-06-16 22:38:58 +0530

    Beta Release

--- simple-alert-plugin.php
+++ simple-alert-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Simple Alert Plugin
  * Plugin URI: http://askaryabbas.com
  * Description: This plugin will add functionality to display alert box based on selected post types.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Askary Abbas
  * Author URI: http://askaryabbas.com
  * Text Domain: simple-alert-box
